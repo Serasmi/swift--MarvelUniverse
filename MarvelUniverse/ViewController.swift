42bb4917b6451127b73ce905721ff31356ac68e5
Serasmi <serasmi@yandex.ru>
2021-08-03 08:45:28 +0300

    refactor: Remove storyboard

--- MarvelUniverse/ViewController.swift
+++ MarvelUniverse/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let testLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Test label"
+        label.tintColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configure()
+    }
+    
+    func configure() {
+        view.backgroundColor = .systemBackground
+        title = "Main VC"
+        
+        view.addSubview(testLabel)
+        
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 
 
